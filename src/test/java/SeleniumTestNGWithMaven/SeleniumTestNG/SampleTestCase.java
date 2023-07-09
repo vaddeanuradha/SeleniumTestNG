600459301688a82c8068e947b62e6d522658c7f6
vaddeanuradha <vaddeanuradha@gmail.com>
2023-07-08 22:37:06 -0700

    adding class files

--- src/test/java/SeleniumTestNGWithMaven/SeleniumTestNG/SampleTestCase.java
+++ src/test/java/SeleniumTestNGWithMaven/SeleniumTestNG/SampleTestCase.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class TestCase1 {
+public class SampleTestCase {
 	@Test
 	public void Testcase1() {
 		System.setProperty("webdriver.chrome.driver", "D:\\Selenium\\chromedriver_win32\\chromedriver.exe");
